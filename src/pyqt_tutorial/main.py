e7d2909495386673d3dd33235ed4668d86f26f58
rfdzan <hekodev.py@gmail.com>
2024-06-14 17:28:29 +0700

    switch to QDialog

--- src/pyqt_tutorial/main.py
+++ src/pyqt_tutorial/main.py
@@ -1,5 +1,7 @@
 import sys
-from PyQt6.QtWidgets import QApplication, QLabel, QMainWindow, QStatusBar, QToolBar, QHBoxLayout, QWidget, QPushButton
+
+
+from PyQt6.QtWidgets import QApplication, QLabel, QDialog, QStatusBar, QToolBar, QHBoxLayout, QWidget, QPushButton
 
 
 class SearchWindow:
@@ -17,18 +19,14 @@ class ViewerWindow:
         self.window = QWidget()
 
 
-class MainWindow(QMainWindow):
+class MainWindow(QDialog):
     def __init__(self):
         super().__init__(parent=None)
         self.setWindowTitle("QMainWindow")
-        self.main_window = QWidget()
         self._createApp()
 
     def _createApp(self):
-        self._createMenu()
-        self._createStatusBar()
-        self.main_window.setLayout(self._createLayout())
-        self.setCentralWidget(self.main_window)
+        self.setLayout(self._createLayout())
 
     def _createLayout(self) -> QHBoxLayout:
         parent_layout = QHBoxLayout()
@@ -36,21 +34,6 @@ class MainWindow(QMainWindow):
         viewer_window = ViewerWindow()
         parent_layout.addWidget(search_window)
         return parent_layout
-
-    def _createMenu(self):
-        # adding '&' -> allows keyboard shortcuts
-        menu = self.menuBar().addMenu("&Menu")
-        menu.addAction("&Exit", self.close)
-
-    def _createToolBar(self):
-        tools = QToolBar()
-        tools.addAction("&Exit", self.close)
-        self.addToolBar(tools)
-
-    def _createStatusBar(self):
-        status = QStatusBar()
-        status.showMessage("I'm the Status Bar")
-        self.setStatusBar(status)
 
 
 if __name__ == "__main__":
